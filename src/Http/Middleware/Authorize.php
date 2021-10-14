ab67862f0368c209f6b16b6afd197b3d5071a14d
Muhammed Sari <muhammed@dive.be>
2021-10-14 12:14:57 +0200

    Add missing type-hints

--- src/Http/Middleware/Authorize.php
+++ src/Http/Middleware/Authorize.php
@@ -2,19 +2,21 @@
 
 namespace Dive\NovaTranslationEditor\Http\Middleware;
 
+use Closure;
 use Dive\NovaTranslationEditor\NovaTranslationEditor;
+use Illuminate\Http\Request;
 use Laravel\Nova\Nova;
 
 class Authorize
 {
-    public function handle($request, $next)
+    public function handle(Request $request, Closure $next)
     {
         $tool = collect(Nova::registeredTools())->first([$this, 'matchesTool']);
 
-        return optional($tool)->authorize($request) ? $next($request) : abort(403);
+        return $tool?->authorize($request) ? $next($request) : abort(403);
     }
 
-    public function matchesTool($tool)
+    public function matchesTool($tool): bool
     {
         return $tool instanceof NovaTranslationEditor;
     }
