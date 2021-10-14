30bbc931ca7e892d07c8fef1d77ffe235b348770
Muhammed Sari <muhammed@dive.be>
2021-10-14 12:14:27 +0200

    Replace hard-coded lang directory

--- src/TranslationManager.php
+++ src/TranslationManager.php
@@ -8,24 +8,26 @@ use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Lang;
+use SplFileInfo;
 
 class TranslationManager
 {
     /**
      * Fetch language files for given locale.
      */
-    public static function getFiles($locale = null) {
+    public static function getFiles(?string $locale = null): Collection
+    {
         $locale ??= config('nova-translation-editor.default_locale');
         $excludeFiles = collect(config('nova-translation-editor.exclude', []));
 
-        return collect(File::allFiles(resource_path('lang/'.$locale)))
-            ->map(fn (\SplFileInfo $file) => ltrim($file->getRelativePath().'/'.$file->getFilenameWithoutExtension(), '/'))
+        return collect(File::allFiles(lang_path($locale)))
+            ->map(fn (SplFileInfo $file) => ltrim($file->getRelativePath().'/'.$file->getFilenameWithoutExtension(), '/'))
             // Filter excluded files
             ->filter(fn ($file) => ! $excludeFiles->contains($file))
             ->flatten();
     }
 
-    public static function getFileLanguageLines($locale, $withValues = true, $file = null): Collection
+    public static function getFileLanguageLines(string $locale, bool $withValues = true, $file = null): Collection
     {
         $files = isset($file) ? collect($file) : self::getFiles($locale);
 
@@ -51,27 +53,28 @@ class TranslationManager
             ->flatten();
     }
 
-    public static function publish() {
-        // Save all translations to files for each locale
-
+    public static function publish()
+    {
         // Fetch default locale translations without values
         $defaultFileTranslations = self::getFileLanguageLines(config('nova-translation-editor.default_locale'), false);
-        $defaultFileTranslationsKeys = $defaultFileTranslations->map(fn ($translation) => $translation->getIdentifier())->toArray();
+        $defaultFileTranslationsKeys = $defaultFileTranslations
+            ->map(fn ($translation) => $translation->unique_id)
+            ->toArray();
 
         foreach (config('nova-translation-editor.supported_locales') as $locale) {
             // Merge with current locale translations & exclude the keys that don't exist in the default locales
             $fileTranslations = self::getFileLanguageLines($locale)
                 ->concat($defaultFileTranslations)
-                ->unique(fn ($translation) => $translation->getIdentifier())
-                ->filter(fn ($translation) => in_array($translation->getIdentifier(), $defaultFileTranslationsKeys));
+                ->unique(fn ($translation) => $translation->unique_id)
+                ->filter(fn ($translation) => in_array($translation->unique_id, $defaultFileTranslationsKeys));
 
             // Merge with DB translations
             $translationGroups = LanguageLine::where('locale', $locale)
                 ->get()
-                ->filter(fn ($translation) => in_array($translation->getIdentifier(), $defaultFileTranslationsKeys))
+                ->filter(fn ($translation) => in_array($translation->unique_id, $defaultFileTranslationsKeys))
                 ->concat($fileTranslations)
-                ->unique(fn ($translation) => $translation->getIdentifier())
-                ->sortBy(fn ($translation) => $translation->getIdentifier())
+                ->unique(fn ($translation) => $translation->unique_id)
+                ->sortBy(fn ($translation) => $translation->unique_id)
                 // Only filter empty values for non-default locale
                 ->filter(fn ($translation) => ($locale == config('nova-translation-editor.default_locale')) ?: $translation->text)
                 ->map(fn ($translation) => $translation->only(['group', 'key', 'text']))
@@ -82,14 +85,14 @@ class TranslationManager
                 $translations = self::unDotArray($translations);
 
                 if (preg_match('/(.*)\/[^\/]+$/', $group, $matches)) {
-                    $path = resource_path('lang/'.$locale.'/'.$matches[1]);
+                    $path = lang_path($locale.'/'.$matches[1]);
 
                     if (! File::isDirectory($path)) {
                         File::makeDirectory($path, 493, true);
                     }
                 }
 
-                File::put(resource_path('lang/'.$locale.'/'.$group.'.php'), '<?php return '.var_export($translations, true).';');
+                File::put(lang_path($locale.'/'.$group.'.php'), '<?php return '.var_export($translations, true).';');
             }
         }
 
@@ -97,9 +100,10 @@ class TranslationManager
         DB::table((new LanguageLine())->getTable())->update(['published_at' => Carbon::now()]);
     }
 
-    private static function unDotArray($dottedArray)
+    private static function unDotArray(array $dottedArray): array
     {
         $array = [];
+
         foreach ($dottedArray as $key => $value) {
             Arr::set($array, $key, $value);
         }
