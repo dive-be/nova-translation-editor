a2479deb3baeb8fc8d6ae9fc0d1d858fa88d4e19
Muhammed Sari <muhammed@dive.be>
2021-10-14 12:13:23 +0200

    Remove useless comments

--- src/NovaTranslationEditor.php
+++ src/NovaTranslationEditor.php
@@ -7,22 +7,12 @@ use Laravel\Nova\Tool;
 
 class NovaTranslationEditor extends Tool
 {
-    /**
-     * Perform any tasks that need to happen when the tool is booted.
-     *
-     * @return void
-     */
     public function boot()
     {
         Nova::script('nova-translation-editor', __DIR__.'/../dist/js/tool.js');
         Nova::style('nova-translation-editor', __DIR__.'/../dist/css/tool.css');
     }
 
-    /**
-     * Build the view that renders the navigation links for the tool.
-     *
-     * @return \Illuminate\View\View
-     */
     public function renderNavigation()
     {
         return view('nova-translation-editor::navigation');
